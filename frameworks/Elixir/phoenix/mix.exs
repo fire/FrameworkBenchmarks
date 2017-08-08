a03a57ba83959e6a08c757dfde9556ca101a1c03
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-08-08 16:24:46 -0700

    Add cockroachdb settings.

--- frameworks/Elixir/phoenix/mix.exs
+++ frameworks/Elixir/phoenix/mix.exs
@@ -28,8 +28,9 @@ defmodule Hello.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.3"},
      {:phoenix_ecto, "~> 3.0"},
+     {:crdbex, git: "https://github.com/fire/crdbex.git", app: false},
+     {:eflame, ~r/.*/, git: "https://github.com/slfritchie/eflame.git", compile: "rebar compile"},
      {:snappydata_ecto, git: "https://github.com/fire/snappydata_ecto.git"},
-     {:postgrex, ">= 0.0.0"},
      {:cowboy, "~> 1.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev}]
